0874415ad9851c605a0f25e1e006009cff16408a
Ameya Yadav <ameyayadav2@gmail.com>
2023-03-25 15:43:33 +0000

    improved function readbility

--- src/components/Signupform/Signup.jsx
+++ src/components/Signupform/Signup.jsx
@@ -13,11 +13,13 @@ function SignupForm() {
     <form onSubmit={handleSubmit}>
       <label>
         Email:
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="email" value={email} 
+        onChange={(e) => setEmail(e.target.value)} />
       </label>
       <label>
         Password:
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input type="password" value={password} 
+        onChange={(e) => setPassword(e.target.value)} />
       </label>
       <button type="submit">Sign Up</button>
     </form>
