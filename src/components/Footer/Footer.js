bda363405cf1782281bd4d52bf7dcdff11ee36b2
Ameya Yadav <ameyayadav2@gmail.com>
2023-03-26 16:58:35 +0100

    updated icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
     return (
+        <>
       <div className="footer-container"> 
         <p >Copyright © 2023 Film Box</p>
-        <h1>Welcome to Open Database Movie Hub </h1>  
+        <h1>Welcome to Open Database Movie Hub </h1>
+        </div> 
+        <div className='Icons'>
+        <a href="#"><FontAwesomeIcon icon={faFacebook} /></a>
+      <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
+      <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
       </div>
+      </>
     );
   }
 
