d1346ca850ff7eb93bf04aa2851cc0d23a980800
Ameya Yadav <ameyayadav2@gmail.com>
2023-03-26 14:12:19 +0100

    updated footer page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Watched } from "./components/Watched";
 import { Add } from "./components/Add";
 import "./App.css";
 import "./lib/font-awesome/css/all.min.css";
+import Footer from "./components/Footer/Footer";
 
 import { GlobalProvider } from "./context/GlobalState";
 
@@ -14,6 +15,7 @@ function App() {
     <GlobalProvider>
       <Router>
         <Header />
+        
 
         <Switch>
           <Route exact path="/">
@@ -27,7 +29,9 @@ function App() {
           </Route>
         </Switch>
       </Router>
+     
     </GlobalProvider>
+   
   );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+    return (
+      <div className="footer-container"> 
+        <p>Copyright © 2023 Film Box</p>
+        <h1>Welcome to Open Database Movie Hub </h1>  
+      </div>
+    );
+  }
+
+  export default Footer;
+  
